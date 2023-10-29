8c57c93f22beaf5fdfab8a16542c82a3a1d13280
Mike Crawford <dev.random.87@gmail.com>
2023-10-29 16:39:32 -0700

    Refactor for modularity

--- Mike/Scripts/get_ports.sh
+++ Mike/Scripts/get_ports.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-if [[ -z "$1" || "$1" == "-h" || "$1" == "--help" ]]; then
-    echo -e "\nUsage: $0 [nmap file] [output directory]"
+# Function to display usage information
+display_usage() {
+    echo -e "\nUsage: $0 -f [nmap file] -o [output directory]"
     exit 1
+}
+
+# Function to process nmap file
+process_nmap_file() {
+    awk '!/#/ && /open/ {split($0, arr, "/"); printf "%s,", arr[1]}' "$1" | sed 's/,$//' >"$2/ports"
+}
+
+nmap_scan=""
+output_dir=""
+
+# Parse args
+while getopts "f:o:h" option; do
+    case "$option" in
+    f) nmap_scan="$OPTARG" ;;
+    o) output_dir="$OPTARG" ;;
+    h) display_usage ;;
+    *) display_usage ;;
+    esac
+done
+
+# Validate call
+if [[ -z "$nmap_scan" || -z "$output_dir" ]]; then
+    display_usage
 fi
 
-nmap_scan="$1"
-output_dir="$2"
-
-awk '!/#/ && /open/ {split($0, arr, "/"); printf "%s,", arr[1]}' "$nmap_scan" | sed 's/,$//' >"${output_dir}/ports"
+# Call main
+process_nmap_file "$nmap_scan" "$output_dir"
