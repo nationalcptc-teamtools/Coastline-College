8ab5e4a1667c6474c9c30483d8319e5ffb150784
Mike Crawford <dev.random.87@gmail.com>
2023-10-31 13:50:05 -0700

    Test openvas setup

--- Mike/Scripts/kali_setup.sh
+++ Mike/Scripts/kali_setup.sh
@@ -291,6 +291,62 @@ install_all_pimp() {
     cleanup
 }
 
+install_openvas() {
+    DOWNLOAD_DIR=/root/greenbone-community-container
+
+    installed() {
+        # $1 should be the command to look for. If $2 is set, we have arguments
+        local failed=0
+        if [ -z "$2" ]; then
+            if ! [ -x "$(command -v "$1")" ]; then
+                failed=1
+            fi
+        else
+            local ret=0
+            "$@" &>/dev/null || ret=$?
+            if [ "$ret" -ne 0 ]; then
+                failed=1
+            fi
+        fi
+
+        if [ $failed -ne 0 ]; then
+            echo "$* is not available. See https://greenbone.github.io/docs/latest/$RELEASE/container/#prerequisites."
+            exit 1
+        fi
+
+    }
+
+    RELEASE="22.4"
+
+    installed curl
+    installed docker
+    installed docker compose
+
+    echo "Using Greenbone Community Containers $RELEASE"
+
+    mkdir -p "$DOWNLOAD_DIR" && cd $DOWNLOAD_DIR || exit
+
+    echo "Downloading docker-compose file..."
+    curl -f -O https://greenbone.github.io/docs/latest/_static/docker-compose-$RELEASE.yml
+
+    echo "Pulling Greenbone Community Containers $RELEASE"
+    docker compose -f "$DOWNLOAD_DIR"/docker-compose-$RELEASE.yml -p greenbone-community-edition pull
+    echo
+
+    echo "Starting Greenbone Community Containers $RELEASE"
+    docker compose -f "$DOWNLOAD_DIR"/docker-compose-$RELEASE.yml -p greenbone-community-edition up -d
+    echo
+
+    read -s -rp "Password for admin user: " password
+    docker compose -f "$DOWNLOAD_DIR"/docker-compose-$RELEASE.yml -p greenbone-community-edition \
+        exec -u gvmd gvmd gvmd --user=admin --new-password="$password"
+
+    echo
+    echo "The feed data will be loaded now. This process may take several minutes up to hours."
+    echo "Before the data is not loaded completely, scans will show insufficient or erroneous results."
+    echo "See https://greenbone.github.io/docs/latest/$RELEASE/container/workflows.html#loading-the-feed-changes for more details."
+}
+
 # Quit function
 reboot_func() {
     read -rp "Would you like to reboot now? (y/N): " reboot_choice
